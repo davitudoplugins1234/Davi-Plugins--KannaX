6924e43d5b3fdc12002b5a4c4b5060be8c9b38d0
Giovanyeeeh <86035674+Giovanyeeeh@users.noreply.github.com>
2022-01-11 19:37:39 -0300

    Create teste.py

---
+++ plugins/teste.py
@@ -0,0 +1,24 @@
+@kannax.on_cmd("sexo", about={"header": "sexoooooooooooo"})
+async def snake_(message: Message):
+    """muito sexo"""
+    out = f"""
+teste1!!
+"""
+
+    out2 = f"""
+teste 2
+"""
+    out3 = f"""
+Perai...é aqui que estão falando de...
+"""
+
+    out4 = f"""
+teste 3
+"""
+    await message.edit(out)
+    await asyncio.sleep(3)
+    await message.edit(out2)
+    await asyncio.sleep(3)
+    await message.edit(out3)
+    await asyncio.sleep(3)
+    await message.edit(out4)
